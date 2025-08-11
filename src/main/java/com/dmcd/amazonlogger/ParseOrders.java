ac2cc99701864b262180206af58bf1adc51e8612
Dan McDermet <dmcd2356@gmail.com>
2025-08-11 09:56:58 -0400

    BUGFIX: wasn't displaying the description from a clipboard file when the description was split into 2 entries separated by a single line with the quantity value.

--- src/main/java/com/dmcd/amazonlogger/ParseOrders.java
+++ src/main/java/com/dmcd/amazonlogger/ParseOrders.java
@@ -210,7 +210,7 @@ public class ParseOrders {
                 bDelDate = true;
             }
             
-            // If we have 2 lines of sufficiet length in a row that match, except
+            // If we have 2 lines of sufficient length in a row that match, except
             //  possibly for the last couple of chars, this must be the description.
             // The 1st line will have a space and the quantity added to it and the
             //  second one won't.
@@ -221,19 +221,31 @@ public class ParseOrders {
                 qtyValue = 1;
                 int linelen = line.length();
                 int prevlen = lastLine.length();
-                if (linelen < DESCRIPT_LEN_MIN && prevlen >= DESCRIPT_LEN_MIN) {
-                    // if we have an intervening line that is simpl a numeric value < 99,
-                    //  let's assume it is the quantity and the next line should be the match for the description.
-                    try {
-                        qtyValue = Integer.valueOf(line);
-                        GUILogPanel.outputInfoMsg (MsgType.DEBUG, "Description followed by Quantity found");
-                        setDescriptionAndQty (line, qtyValue, newOrder);
-                        lastLine = "";
-                        continue;
-                    } catch (NumberFormatException exMsg) {
-                        // not a numeric, must stick to prev rule of 2 matching line
+                int lineValue = 0;
+                
+                // check if the quantity was put on a separate line between the 2 lines of description.
+                // if we have an intervening line that is simply a numeric value <= 99,
+                //  let's assume it is the quantity and the next line should be the match for the description.
+                // if it's not a numeric, we stick to prev rule of 2 matching line.
+                if (linelen <= 2) { // Qty will be range of 2 - 99
+                    lineValue = isLineAnInteger(line);
+                    if (lineValue > 0) {
+                        line = readLine();
+                        if (line == null) {
+                            break;
+                        }
+                        keywordInfo = new OrderInfo(line);
+                        if (Keyword.KeyTyp.NONE == keywordInfo.orderId) {
+                            linelen = line.length();
+                            qtyValue = lineValue;
+                            GUILogPanel.outputInfoMsg (MsgType.DEBUG, "Description followed by Quantity found: " + qtyValue);
+                        } else {
+                            // else, next line was a keyword - indicate this is not a description
+                            linelen = 0;
+                        }
                     }
                 }
+
                 // make sure we had 2 NOT_FOUND lines in a row
                 if (linelen >= DESCRIPT_LEN_MIN && prevlen >= DESCRIPT_LEN_MIN) {
                     // eliminate the extraneous material from the non-truncated entry
@@ -250,18 +262,21 @@ public class ParseOrders {
                         continue;
                     }
                     // else, see if there is a qty number attached to the 1st entry
-                    int offset = lastLine.lastIndexOf(' ');
-                    if (prevlen < offset + 3) { // this would allow for a space & 2 digits for qty
-                        String qtystr = lastLine.substring(offset).strip();
-                        try {
-                            qtyValue = Integer.valueOf(qtystr);
-                            if (qtyValue < 1 || qtyValue > 100) {
-                                GUILogPanel.outputInfoMsg (MsgType.DEBUG, "Invalid range of Qty value: " + qtystr);
+                    int offset = linelen;
+                    if (qtyValue == 1) {
+                        offset = lastLine.lastIndexOf(' ');
+                        if (prevlen < offset + 3) { // this would allow for a space & 2 digits for qty
+                            String qtystr = lastLine.substring(offset).strip();
+                            try {
+                                qtyValue = Integer.valueOf(qtystr);
+                                if (qtyValue < 1 || qtyValue > 100) {
+                                    GUILogPanel.outputInfoMsg (MsgType.DEBUG, "Invalid range of Qty value: " + qtystr);
+                                    qtyValue = 1;
+                                }
+                            } catch (NumberFormatException ex) {
+                                GUILogPanel.outputInfoMsg (MsgType.DEBUG, "Last word of Description was not an Integer value: " + qtystr);
                                 qtyValue = 1;
                             }
-                        } catch (NumberFormatException ex) {
-                            GUILogPanel.outputInfoMsg (MsgType.DEBUG, "Last word of Description was not an Integer value: " + qtystr);
-                            qtyValue = 1;
                         }
                     }
                     // check if 2nd line is an exact substring of 1st line
@@ -357,6 +372,27 @@ public class ParseOrders {
         return amazonList;
     }
 
+    /**
+     * determines if a line consists of a single numeric value.
+     * 
+     * @param line   - the line to examine
+     * 
+     * @return the value of the line entry, or 0 if line was not correct integer format
+     */
+    private static int isLineAnInteger (String line) {
+        int value = 0;
+        int lineLen = line.length();
+        if (lineLen == 0 || line.charAt(0) == '-') {
+            return 0;
+        }
+        try {
+            value = Integer.parseInt(line);
+        } catch (NumberFormatException exMsg) {
+            return 0;
+        }
+        return value;
+    }
+    
     /**
      * saves the description and quantity for the current item in the order.
      * if the description is already defined for the order, it creates a new entry.
